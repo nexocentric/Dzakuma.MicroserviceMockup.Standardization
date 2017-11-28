580e224ca798100df253f81be654404134f617b6
Dodzidenu Y. Dzakuma <nexocentric@yahoo.com>
2017-11-27 21:22:35 -0600

    Updated the assembly files to plug into build automation.

--- Dzakuma.MicroserviceMockup.Standardization.Tests/Properties/AssemblyInfo.cs
+++ Dzakuma.MicroserviceMockup.Standardization.Tests/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dzakuma.MicroserviceMockup.Standardization.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A set of tests for the mock standardization framework.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("None")]
 [assembly: AssemblyProduct("Dzakuma.MicroserviceMockup.Standardization.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Dodzidenu Dzakuma 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
